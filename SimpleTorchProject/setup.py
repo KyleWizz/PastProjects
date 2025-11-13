248b691700e9aa8ac4c9ba93a62004cde7c10054
Kyle <138802402+KyleWizz@users.noreply.github.com>
2025-11-13 23:32:40 +0900

    d

---
+++ SimpleTorchProject/setup.py
@@ -0,0 +1,20 @@
+#downloads file
+
+#i'll get around to fully creating this file for downloads,
+#right now am focusing on data cleaning + getting data prepared for training.
+
+import requests
+
+url = "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/405/GCF_000001405.26_GRCh38/GCF_000001405.26_GRCh38_feature_table.txt.gz"
+
+print("Downloading feature table...")
+response = requests.get(url)
+
+with open("data/feature_table.txt.gz", 'wb') as f:
+    f.write(response.content)
+
+print("✓ Downloaded!")
+#wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/405/GCF_000001405.26_GRCh38/GCF_000001405.26_GRCh38_feature_table.txt.gz
+
+# Or using curl
+#curl -O https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/405/GCF_000001405.26_GRCh38/GCF_000001405.26_GRCh38_feature_table.txt.gz
