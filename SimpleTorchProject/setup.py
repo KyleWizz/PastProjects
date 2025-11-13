ea42639914c3c1ff7c4f2e9771e8d449673fd744
Kyle <138802402+KyleWizz@users.noreply.github.com>
2025-11-13 23:35:31 +0900

    Add files via upload

--- SimpleTorchProject/setup.py
+++ SimpleTorchProject/setup.py
@@ -1,6 +1,6 @@
 #downloads file
 
-#i'll get around to fully creating this file for downloads,
+#i'll get around to fully creating this file for downloads, or push them to git
 #right now am focusing on data cleaning + getting data prepared for training.
 
 import requests
@@ -13,7 +13,7 @@ response = requests.get(url)
 with open("data/feature_table.txt.gz", 'wb') as f:
     f.write(response.content)
 
-print("✓ Downloaded!")
+print("Downloaded!")
 #wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/405/GCF_000001405.26_GRCh38/GCF_000001405.26_GRCh38_feature_table.txt.gz
 
 # Or using curl
